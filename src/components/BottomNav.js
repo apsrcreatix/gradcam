af26c340eb13f130f997ee2188d2f6122e328788
Aditya Pratap Singh Rajput <aditya@goscale.co>
2019-07-01 17:23:31 +0530

    add bottom nav

---
+++ src/components/BottomNav.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+function BottomNav() {
+  return (
+    <div>
+  <div className="hero-foot">
+    <nav className="tabs is-boxed is-fullwidth">
+      <div className="container">
+        <ul>
+          <li className="is-active"><a>Home</a></li>
+          <li><a>Settings</a></li>
+          <li><a>About</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+    </div>
+  );
+}
+
+export default BottomNav;
