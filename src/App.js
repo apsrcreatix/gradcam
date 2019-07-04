526c6bbd261631729d67a7cda79c7dfc23b619a4
Aditya Pratap Singh Rajput <aditya@goscale.co>
2019-07-04 13:48:04 +0530

    add half baked seeds

--- src/App.js
+++ src/App.js
@@ -20,19 +20,21 @@ export default class App extends React.Component {
       type: "linear-gradient",
       style: "",
       dayFlag: true,
+      currentSeed: ""
     }
     this.firstCall.bind(this);
     this.handleChange.bind(this);
     this.componentDidMount.bind(this);
-  
+   
     
   }
 
   firstCall() {
-    fetch(`https://api.noopschallenge.com/hexbot?count=${this.state.count}`, {
+    fetch(`https://api.noopschallenge.com/hexbot?count=${this.state.count}${this.state.currentSeed!==""?("&seed="+this.state.currentSeed+","+this.state.currentSeed+","+this.state.currentSeed):""}`, {
       method: 'GET'
     })
       .then(response => {
+        console.log(`https://api.noopschallenge.com/hexbot?count=${this.state.count}${this.state.currentSeed!==""?("&seed="+this.state.currentSeed+","+this.state.currentSeed+","+this.state.currentSeed):""}`);
         return response.json();
       })
       .then(data => {
@@ -48,15 +50,24 @@ export default class App extends React.Component {
       
   }
   
-  componentDidMount() {
-    this.firstCall();
+ vanillaTilt() {
     VanillaTilt.init(document.querySelectorAll(".dynamic-shadow"),{
       max:25,
       speed:500,
       glare:true,
-      scale: 1.2,
-      'max-glare':0.5
-    });
+      scale: 1.5,
+      'max-glare':0.5,
+      transition: true,
+      perspective: 1000,
+      easing: "cubic-bezier(.03,.98,.52,.99)",
+      gyroscope: true   
+    })
+  }
+
+  componentDidMount() {
+    this.vanillaTilt();
+    this.firstCall();
+   
   }
 
   handleChange(e) {
@@ -74,6 +85,13 @@ export default class App extends React.Component {
   handleActiveOption(e) {
     this.setState({
     activeOption : e.target.outerText
+    })
+    this.vanillaTilt();
+  }
+  
+  handleColor(e){
+    this.setState({
+      currentSeed: e.target.value.slice(1)
     })
   }
 
@@ -100,7 +118,11 @@ export default class App extends React.Component {
       handleSelect={(e) => this.handleSelect(e)} 
       submitCount={() => this.firstCall()}
       currentType={this.state.type}
-      />: <section>
+      currentSeed={this.state.currentSeed}
+      handleColor={(e)=>this.handleColor(e)}
+      />
+      : 
+      <section>
         <h1 className="is-size-1 is-family-sans-serif has-text-weight-bold">Love Gradients?</h1>
         <img src={this.state.dayFlag?day:night} width="300xp" height="300px" alt="This shows day or night!" className="dynamic-shadow"/>
         <p>If you want to learn about different type of gradients and experiment with them, then you are right place.</p>
@@ -111,7 +133,7 @@ export default class App extends React.Component {
   </div>
 
   {/* <!-- Hero footer: will stick at the bottom --> */}
-  <BottomNav activeOption={this.state.activeOption} handleActiveOption={(e)=>this.handleActiveOption(e)}/>
+  <BottomNav  activeOption={this.state.activeOption} handleActiveOption={(e)=>this.handleActiveOption(e)} />
 </section>
     )
   }
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -18,38 +18,48 @@ function Settings(props) {
                 <h1 className="title">
                     Settings
                 </h1>
-                <p className="subtitle" style={{ whiteSpace: 'nowrap',width: '300px',overflow: 'hidden',textOverflow: 'ellipsis'}}>
-                    {props.colors.map((value, index, array) => <span key={`${value}`}><span style={{ background: value }}>{value}</span>{(array.length === index + 1) ? "." : ","}</span>)}
-                </p>
+                <h6 className="subtitle" style={{ whiteSpace: 'nowrap',width: '300px',overflow: 'hidden',textOverflow: 'ellipsis'}}>
+                    {props.colors.map((value, index, array) => <span key={`${index}`}><span style={{ color: value }}>{value}</span>{(array.length === index + 1) ? "." : ","}</span>)}
+                </h6>
     
                     <div className="field has-addons">
-                        <p className="control ">
+                        <div className="control ">
                             <input id="noc" style={STYLE.inputs} name="count" min="2" max="1000" className="input" type="number" placeholder={"Current count is "+props.count} onChange={props.handleChange} />
-                        </p>
-                        <p className="control">
+                        </div>
+                        <div className="control">
                             <button  className="button" onClick={props.submitCount} disabled={props.count>1?false:true}>
                                 Set
                             </button>
-                        </p>
+                        </div>
                     </div>
                 <div className="field">
-                <p className="control ">
+                <div className="control ">
                 <div className="select" >
                     <select name="type" id="tog" onChange={props.handleSelect}>
                         <option value={props.currentType} >{props.currentType}</option>
                         {(GRADIENT_TYPE.filter((value1)=>value1!==props.currentType)).map((value)=><option key={value} value={value}>{value}</option>)}
                     </select>
                 </div>
-                </p>    
+                </div>    
                 </div>
                 <div className="field">
-                <p className="control">
+                <div className="control ">
+                    <input type="color" name="colorpicker" id="cpi" onChange={props.handleColor}/>
+                    <h4>
+                    {props.currentSeed!==null?<span style={{color: "#"+props.currentSeed }}>{props.currentSeed!==""?"#"+props.currentSeed:""}</span>:""}
+                    </h4>
+                    <div className="is-size-7">Provide a seed color.</div>
+
+                </div>    
+                </div>
+                <div className="field">
+                <div className="control">
                         <label className="checkbox">
                             <input type="checkbox" />
                              Color Interactivity
                             </label>
-                            <p className="is-size-7">If you touch the box, colors will change.</p>
-                </p>    
+                            <div className="is-size-7">If you touch the box, colors will change.</div>
+                </div>    
                 </div>
             </div>
       
